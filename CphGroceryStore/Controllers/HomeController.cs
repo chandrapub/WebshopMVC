2bbfe545cd9923350f45834c8ab81667e4c93818
Shekhar <chandrapub@gmail.com>
2019-03-17 15:20:30 +0100

    upload latest version

--- CphGroceryStore/Controllers/HomeController.cs
+++ CphGroceryStore/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CphGroceryStore.Models;
+using groceryStore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,38 @@ namespace CphGroceryStore.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(string searchCriteria)
         {
-            return View();
+            var factory = new ShopFactory();
+            IQueryable<Product> prods = factory.Products.OrderBy(p => p.Name);
+            if (searchCriteria != null)
+            {
+                prods = prods.Where(p => p.Name.Contains(searchCriteria));
+            }
+            var products = prods.Take(10).ToList();
+            return View(products);
         }
+
+        public ActionResult Picture(int id)
+        {
+            var factory = new ShopFactory();
+            var product = factory.Products.Where(p => p.ID == id).FirstOrDefault();
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            var img = new System.Web.Helpers.WebImage(string.Format("~/Content/images/{0}.jpg", product.ImageName));
+            img.Resize(100, 50);
+            return File(img.GetBytes(), "image/jpeg");
+        }
+
+        public ActionResult Details(int id)
+        {
+            var factory = new ShopFactory();
+            var found = factory.Products.Where(p => p.ID == id).FirstOrDefault();
+            return View(found);
+        }
+
 
         public ActionResult About()
         {
